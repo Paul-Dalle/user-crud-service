d51c93f9165409bac9335c3d6f0c1baa4cfb34b7
Paul-Dalle <praju@equiwiz.com>
2025-02-18 13:41:40 +0530

    Delete user-crud-service directory

--- user-crud-service/user-crud-service/src/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://admin:password@localhost:5432/postgres')
